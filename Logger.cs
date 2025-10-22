727e375dfcd6b4d59a624e70dc29095c9b35ea79
faolanbig <faolan.big@web.de>
2025-10-22 12:56:39 +0200

    adding prefix and preprefix config and basic timestamp config

--- Logger.cs
+++ Logger.cs
@@ -12,19 +12,30 @@ namespace BigLog
         public Logger() { }
         public Logger(Logger loggerImport) 
         {
+            // color import
             ColorInf = loggerImport.ColorInf;
             ColorSuc = loggerImport.ColorSuc;
             ColorWar = loggerImport.ColorWar;
             ColorErr = loggerImport.ColorErr;
             Color_fallback = loggerImport.Color_fallback;
             EnableDefaultColors = loggerImport.EnableDefaultColors;
+
+            // format import
+            PrefixInf = loggerImport.PrefixInf;
+            PrefixSuc = loggerImport.PrefixSuc;
+            PrefixWar = loggerImport.PrefixWar;
+            PrefixErr = loggerImport.PrefixErr;
         }
 
+        //////////////////////////////
+        /// PRINTING-ZONE-TERMINAL ///
+        //////////////////////////////
 
-          ///////////////////
-         /// COLOR-ZONE ////
-        ///////////////////
-        
+
+        //////////////////
+        /// COLOR-ZONE ///
+        //////////////////
+
         private ConsoleColor color_fallback = ConsoleColor.White;
         public ConsoleColor Color_fallback
         {
@@ -35,34 +46,24 @@ namespace BigLog
                 setDefaultColors_fallback();
             }
         }
-        public readonly ConsoleColor ColorSuc_default = ConsoleColor.Green;
-        public readonly ConsoleColor ColorWar_default = ConsoleColor.Yellow;
-        public readonly ConsoleColor ColorErr_default = ConsoleColor.Red;
-        public readonly ConsoleColor ColorInf_default = ConsoleColor.White;
+        public static readonly ConsoleColor ColorSuc_default = ConsoleColor.Green;
+        public static readonly ConsoleColor ColorWar_default = ConsoleColor.Yellow;
+        public static readonly ConsoleColor ColorErr_default = ConsoleColor.Red;
+        public static readonly ConsoleColor ColorInf_default = ConsoleColor.White;
 
-        public ConsoleColor ColorInf = ConsoleColor.White;
-        public ConsoleColor ColorSuc = ConsoleColor.White; //success
-        public ConsoleColor ColorWar = ConsoleColor.White; //warning
-        public ConsoleColor ColorErr = ConsoleColor.White;
+        public ConsoleColor ColorInf = ColorSuc_default;
+        public ConsoleColor ColorSuc = ColorWar_default; //success
+        public ConsoleColor ColorWar = ColorErr_default; //warning
+        public ConsoleColor ColorErr = ColorInf_default;
         private bool enableDefaultColors = false;
         public bool EnableDefaultColors
         {
-            get
-            {
-                return enableDefaultColors;
-            }
+            get { return enableDefaultColors; }
             set
             {
-                if (value)
-                {
-                    setDefaultColors();
-                    enableDefaultColors = value;
-                }
-                else
-                {
-                    setDefaultColors_fallback();
-                    enableDefaultColors = value;
-                }
+                if (value) { setDefaultColors(); }
+                else { setDefaultColors_fallback(); }
+                enableDefaultColors = value;
             }
         }
         private void setDefaultColors()
@@ -80,21 +81,65 @@ namespace BigLog
             ColorErr = color_fallback;
         }
 
+        ///////////////////
+        /// FORMAT-ZONE ///
+        ///////////////////
 
-          //////////////////////////////
-         /// PRINTING-ZONE-TERMINAL ///
-        //////////////////////////////
-        
+        public static readonly string PrePrefix_default = "logger: ";
+        public string PrePrefix = PrePrefix_default;
+
+        public static readonly string PrefixInf_default_short = "inf";
+        public static readonly string PrefixSuc_default_short = "suc";
+        public static readonly string PrefixWar_default_short = "war";
+        public static readonly string PrefixErr_default_short = "err";
+
+        public static readonly string PrefixInf_default_long = "info";
+        public static readonly string PrefixSuc_default_long = "success";
+        public static readonly string PrefixWar_default_long = "warning";
+        public static readonly string PrefixErr_default_long = "error";
+
+        public string PrefixInf = PrefixInf_default_short;
+        public string PrefixSuc = PrefixSuc_default_short;
+        public string PrefixWar = PrefixWar_default_short;
+        public string PrefixErr = PrefixErr_default_short;
+
+        private bool useShortPrefix = true;
+        public bool UseShortPrefix
+        {
+            get { return useShortPrefix; }
+            set
+            {
+                if (value) { setPrefix_short(); }
+                else { setPrefix_long(); }
+                    useShortPrefix = value;
+            }
+        }
+        public bool PrintTimeStamp = true;
+        public bool PrintTimeStampBeforeLevel = true;
+        private void setPrefix_short()
+        {
+            PrefixInf = PrefixInf_default_short;
+            PrefixSuc = PrefixSuc_default_short;
+            PrefixWar = PrefixWar_default_short;
+            PrefixErr = PrefixErr_default_short;
+        }
+        private void setPrefix_long()
+        {
+            PrefixInf = PrefixInf_default_long;
+            PrefixSuc = PrefixSuc_default_long;
+            PrefixWar = PrefixWar_default_long;
+            PrefixErr = PrefixErr_default_long;
+        }
+
         public void toTerm(string text)
         {
             PrintToTerminal.ToTerm(this, text);
         }
 
+        //////////////////////////
+        /// PRINTING-ZONE-FILE ///
+        //////////////////////////
 
-          /////////////////////////
-         /// PRINTING-ZONE-FILE //
-        /////////////////////////
-        
         public void toFile(string text)
         {
             PrintToFile.ToFile(this, text);
